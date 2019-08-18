8d28a67615e2eb581008378775675da14dec542e
Milena Kari <milenakari@yahoo.com>
2019-08-18 19:03:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,17 +9,12 @@ function findMatching(drivers, str) {
   return matching;
 }
 
-function fuzzyMatch(drivers, str) {
-  let matching = [];
-  let n = str.length;
-  for (const driver of drivers) {
-    if (driver.slice(0,n).toLowerCase() === str.toLowerCase()) {
-      matching.push(driver);
-    }
-  }
-  return matching;
+function fuzzyMatch(list, name) {
+  return list.filter(function (driverName) {
+    return driverName.slice(0, name.length).toLowerCase() === name.toLowerCase();
+  })
 }
-
+  
 function matchName(drivers, name) {
   let matching = [];
   for (const driver of drivers) {
